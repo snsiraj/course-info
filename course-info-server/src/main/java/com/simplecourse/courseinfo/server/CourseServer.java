c5435d8e5460e7ebc711a04ac522503acb1ddd17
Siraj Mohideen <s.n.siraj@gmail.com>
2023-03-13 20:44:24 -0400

    Added Maven Dependency management setting, updated the build with shading.

--- course-info-server/src/main/java/com/simplecourse/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/simplecourse/courseinfo/server/CourseServer.java
@@ -5,16 +5,37 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
+import java.util.logging.LogManager;
 
 public class CourseServer {
+    static {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+    }
     private static final Logger LOG= LoggerFactory.getLogger(CourseServer.class);
     private static final String BASE_URI="http://localhost:7070/";
     public static void main(String... args){
         LOG.info("Starting HTTP Server...");
-        CourseRepo repo=CourseRepo.getCourseRepo("./courses.db");
+        String databaseFileName=loadDatabaseFileName();
+        CourseRepo repo=CourseRepo.getCourseRepo(databaseFileName);
         ResourceConfig config=new ResourceConfig().register(new CourseResource(repo));
         GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI),config);
+    }
+
+    private static String loadDatabaseFileName() {
+        try(InputStream propertiesStream=CourseServer.class.getResourceAsStream("/server.properties")){
+            Properties properties=new Properties();
+            properties.load(propertiesStream);
+            return properties.getProperty("course-info.database");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
